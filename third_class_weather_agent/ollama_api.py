9e4da0deede0be428232573e17a4bf26121f4832
priyansudash1999 <1999priyansudash@gmail.com>
2025-05-01 22:10:41 +0530

    persona pf hitesh sir

--- third_class_weather_agent/ollama_api.py
+++ third_class_weather_agent/ollama_api.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
 from ollama import Client
+from fastapi import Body
 
 app = FastAPI()
-client = Client(
-  host='http://localhost:11434'
-)
+
+client = Client(host='http://localhost:11434')
 
 client.pull('gemma3:1b')
+
 @app.post("/chat")
+def chat(message: str = Body(..., description= "Chat Message")):
+    try:
+        response = client.chat(model="gemma3:1b", messages=[
+            {"role": "user", "content": "Hey there"}
+        ])
+    
+        print("Response from Ollama: ", response)
 
-def chat():
-  response = client.chat(model="gemma3:1b", messages=[
-    {"role": "user", "content": "Hey there"}
-  ])
-
-  return response['messages']['content']
+        if 'message' in response:
+            message = response['message']
+            print(message) 
+            
+            return {"response": message.content}
+        else:
+            return {"error": "No 'message' found in response"}
+    
+    except Exception as e:
+        # Catch and log any errors
+        return {"error": str(e)}
